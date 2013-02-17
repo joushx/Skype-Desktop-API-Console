608bef4e2d77588a4ada88654ed12fb175b6bc9d
Johannes Mittendorfer <johannes.mittendorfer@gmail.com>
2013-02-17 17:01:54 +0100

    initial commit

---
+++ SkypeAPIConsole.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2013-02-17T16:29:30
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT += dbus
+
+QT       -= gui
+
+TARGET = SkypeAPIConsole
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <QCoreApplication>
+#include <QtDBus/QDBusConnection>
+#include <QtDBus/QDBusInterface>
+#include <QtDBus/QDBusReply>
+#include <QTextStream>
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    QTextStream cout(stdout);
+    QTextStream cin(stdin);
+
+    if (QDBusConnection::sessionBus().isConnected()) {
+        QString skypeServiceName("com.Skype.API");
+        QDBusInterface iface(skypeServiceName, "/com/Skype", "com.Skype.API", QDBusConnection::sessionBus());
+
+        if (iface.isValid()) {
+
+            cout << "OK: Connected to DBus" << endl;
+            cout << "Enter Application Name:" << endl;
+
+            QDBusReply<QString> reply = iface.call("Invoke", "NAME " + cin.readLine());
+
+            if (reply.isValid()) {
+
+                if(reply.value() == "OK"){
+
+                    cout << "OK: Connection successfull" << endl;
+
+                    cout << "Enter Protocol Version (1-8) eg. 5:" << endl;
+
+                    // specify the protocol
+                    reply = iface.call("Invoke", "PROTOCOL " + cin.readLine());
+
+                    if (reply.isValid()) {
+
+                        cout << "OK: Setting protocol version" << endl;
+
+                        cout << "Enter command(s):" << endl;
+
+                        command:
+
+                        reply = iface.call("Invoke", cin.readLine());
+                        cout << reply.value() << endl;
+
+                        goto command;
+                    }
+                }
+                else{
+                    cout << "Error: Connection rejected" << endl;
+                }
+            }
+            else{
+                cout << "Error: Connection failed" << endl;
+            }
+        }
+    }
+    else{
+        cout << "Error: Cannot establish DBus connection!" << endl;
+    }
+    
+    return a.exec();
+}
